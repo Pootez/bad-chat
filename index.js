781ac200ca6555325aef705e5e73e91e957369da
Pootez <sondrenoe@gmail.com>
2022-09-06 15:12:05 +0200

    1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let messages = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.post("/messages", (req, res) => {
+  console.log("hm", req.body);
+  if (req.body.name && req.body.message) {
+    messages.push({ name: req.body.name, message: req.body.message });
+    res.send().status(200);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
