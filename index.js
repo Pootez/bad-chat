f64ac94e0e11072eb42afafefa72581175875080
Sondre Espeland <102857059+Pootez@users.noreply.github.com>
2022-10-04 12:27:19 +0200

    Update port

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.post("/messages", (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
